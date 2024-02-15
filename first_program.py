cb82d3ef2c882433bc76e604d952c689cd001bcc
lioneltashi <02230101.cst@rub.edu.bt>
2024-02-15 10:51:18 +0600

    first_program.py

--- first_program.py
+++ first_program.py
@@ -2,4 +2,5 @@ print("kuzu")
 print("hello and I am a student of cst")
 print("my trail")
 print("greatest of all time")
+print("are we going to learn just print and print? why not c sharp language")
 
