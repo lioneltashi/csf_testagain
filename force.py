41533a63fbd593d3780c78f7d739b648120fe8f1
lioneltashi <02230101.cst@rub.edu.bt>
2024-02-19 15:01:33 +0600

    force.py

---
+++ force.py
@@ -0,0 +1,32 @@
+# learning
+print("Newton's second law calculations") 
+print("select a value:")
+print("1. Mass (m)")
+print("2. Accleration (a)")
+print("3. Force (f)")
+
+users_choice= input("Enter an above option number:")
+if users_choice == "1":
+    a= float(input("Enter the acceleration:"))
+    f= float(input("Enter force:"))
+    m= f/a
+    print(f"Mass(m)={m}")
+
+elif users_choice == "2":
+    f= float(input("Enter the force:"))
+    m= float(input("Enter mass:"))
+    a= f/m
+    print(f"Acceleration(a)={a}")
+
+elif users_choice == "3":
+    a= float(input("Enter acceleration:"))
+    m= float(input("Enter mass:"))
+    f= m*a
+    print(f"Force(f)={f}")
+
+else:
+    print("Please select the above option number:")
+
+
+
+
