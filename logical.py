f2faa53eb8b75da77360c831cdfef887333367d7
lioneltashi <02230101.cst@rub.edu.bt>
2024-02-26 14:43:57 +0600

    logical

---
+++ logical.py
@@ -0,0 +1,17 @@
+x = 5
+print(x > 3 and x < 10)
+
+y = 12
+print(y > 10 and y % 5 == 0)
+
+
+print(x > 3 or x < 10)
+
+
+print(y > 10 or y % 5 == 0)
+
+
+print(x > 3 and x < 10)
+
+
+print(y > 10 and y % 5 == 0)
