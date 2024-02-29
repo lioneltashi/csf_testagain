d43fda7cda71b6d789165e1605d1157dfa4e309a
lioneltashi <02230101.cst@rub.edu.bt>
2024-02-29 11:27:42 +0600

    exercise2

---
+++ exercise2.py
@@ -0,0 +1,14 @@
+name = input("Enter your name:")
+num1 = float(input("Enter a number:"))
+num2 = float(input("Enter a number:")) 
+
+addition_result = (num1 + num2)
+subtraction_result = (num1 - num2)
+multiplication_result = (num1 * num2)
+division_result = (num1 / num2)
+
+
+print("the sum of your numbers is",addition_result)
+print("the subtraction of your numbers is",subtraction_result)
+print("the multiplication of your numbers is",multiplication_result)
+print("the division of your numbers is",division_result)
