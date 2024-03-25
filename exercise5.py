dec2b1e065368cb4ab27b33037e6148a73ac3fdd
lioneltashi <02230101.cst@rub.edu.bt>
2024-03-25 13:20:58 +0600

    exercise5

--- exercise5.py
+++ exercise5.py
@@ -15,5 +15,16 @@ for i in range(1,height + 1):
 
 # part 3
 
+for i in range(1,10):
+    if i == 3:
+        print(f"skipping {i}")
+        continue
+    elif i == 7:
+        print(f"reached {i} breaking loop")
+        break
+print(i)
+
+    
+
 
 
